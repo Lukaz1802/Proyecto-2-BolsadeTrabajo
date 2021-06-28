eacb44ef0fc3d8f40c605500a3a0c6fc4dcfa78d
Lucas Lopez <lukaz1802.ll@gmail.com>
2021-06-28 00:16:51 -0300

    agrego registro de usuarios

---
+++ registro.js
@@ -0,0 +1,33 @@
+let usuarios = JSON.parse (localStorage.getItem ("usuarios")) || []
+
+let usuario = {
+    id: new Date().getTime(),
+    email: "",
+    contrasena: "",
+    nombre: "",
+    fotoPerfil:"",
+};
+
+let formulario = document.querySelector("form")
+
+const handleChange = function (e) {
+    usuario = {
+      ...usuario,
+      [e.target.name]: e.target.value,
+    };
+  };
+
+  const handleSubmit = function (e) {
+    e.preventDefault();
+  
+    if (usuario.nombre && usuario.email && usuario.contrasena) {
+      usuarios.push(usuario);
+      localStorage.setItem("usuarios", JSON.stringify(usuarios));
+      formulario.reset();
+  
+      //traer los campos de la card con los datos a mostrar
+      document.querySelector("#name").innerText = usuario.nombre;
+      document.querySelector("#correo").innerText = usuario.email;
+      document.querySelector(".img_card").src = usuario.imagen;
+    }
+  };
