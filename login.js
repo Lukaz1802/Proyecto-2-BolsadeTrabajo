258cab79c30b7b20a7697d216b12c0d82468763e
Pablo Giroud <pablogiroud@gmail.com>
2021-06-30 21:55:59 -0300

    reparacion de branch dev

--- login.js
+++ login.js
@@ -24,7 +24,7 @@ const handleChange = function (e) {
         alert("Usted puede ingresar a la página");
         localStorage.setItem("usuario", JSON.stringify(usuarioVerificado));
   
-        location.replace("/home.html")
+        location.replace("/index.html")
         
       } else {
         alert("usuario o contraseña incorrectos");
