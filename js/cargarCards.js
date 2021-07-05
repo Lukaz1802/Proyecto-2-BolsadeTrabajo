951e92092ad0fb6dfc3f2ceba9a9af8e3216c09a
pgiroud <pgiroud@gruposabra.com>
2021-07-05 02:37:34 -0300

    avance con postulacion de usuario y carga de tabla

--- js/cargarCards.js
+++ js/cargarCards.js
@@ -16,10 +16,12 @@ const cargarCard=function(){
              <p>Tipo: ${item.tipo}</p>
              <p>Ubicación: ${item.ubicacion}</p>
              <p>Página Web: ${item.url}</p>
+             <label>ID de empleo:</label>
+             <p id="idEmpleo">${item.id}</p>
              <p class="card-text"><small class="text-muted">
              ${item.fechaCreacion}</small></p>
              <div id="botonPost">
-           <button type="button" class="btn  mb-4 " data-toggle="button" >
+           <button type="button" class="btn  mb-4 " data-toggle="button" onclick="agregarPostulante()">
              Postularse  </button></div>
            </div>
            </div>
@@ -30,3 +32,31 @@ const cargarCard=function(){
       });
 }
 cargarCard()
+
+const agregarPostulante=function(){
+  let idEmpleo = document.getElementsByTagName('p')[5].innerHTML;
+
+  console.log(idEmpleo);
+  for (i=0; i < trabajos.length; i++) {
+    console.log(trabajos[i].id)
+    console.log(idEmpleo)
+    if(trabajos[i].id == idEmpleo ){
+      console.log(trabajos[i].vacantes)
+      if(trabajos[i].vacantes>0){
+        trabajos[i].postulantes.push(usuario.id);
+        localStorage.setItem("trabajo",JSON.stringify(trabajos))
+        alert(`Se cargaron correctamente los datos\n\nTe has postulado correctamente en el empleo ${idEmpleo}`) 
+      }else{
+            alert("Vacante tiene que ser mayor que 0")
+      }
+    }
+  }
+}
+
+/*
+Falta:
+validar que no duplique postulacion
+let idEmpleo capture el id correspondiente a la card que se desa postular
+
+*/
+agregarPostulante()
