8043d2613d97c7bf498d0b8ed09f8911e0d363ea
Pablo Giroud <pablogiroud@gmail.com>
2021-07-05 17:10:09 -0300

    intento 2

--- js/cargarCards.js
+++ js/cargarCards.js
@@ -32,31 +32,3 @@ const cargarCard=function(){
       });
 }
 cargarCard()
-
-const agregarPostulante=function(){
-  let idEmpleo = document.getElementsByTagName('p')[5].innerHTML;
-
-  console.log(idEmpleo);
-  for (i=0; i < trabajos.length; i++) {
-    console.log(trabajos[i].id)
-    console.log(idEmpleo)
-    if(trabajos[i].id == idEmpleo ){
-      console.log(trabajos[i].vacantes)
-      if(trabajos[i].vacantes>0){
-        trabajos[i].postulantes.push(usuario.id);
-        localStorage.setItem("trabajo",JSON.stringify(trabajos))
-        alert(`Se cargaron correctamente los datos\n\nTe has postulado correctamente en el empleo ${idEmpleo}`) 
-      }else{
-            alert("Vacante tiene que ser mayor que 0")
-      }
-    }
-  }
-}
-
-/*
-Falta:
-validar que no duplique postulacion
-let idEmpleo capture el id correspondiente a la card que se desa postular
-
-*/
-agregarPostulante()
